595d395df61865ba922dd8e82dffccaa56ee1d89
Aakash PR <aakashpr1c2alo@gmail.com>
2023-02-04 11:19:17 +0530

    completed get the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+  let extractedValue = arr.map((book) => book.title);
+  return extractedValue;
 };
 
 // Do not edit below this line
